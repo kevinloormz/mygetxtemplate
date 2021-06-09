82b913d451ecfc48ca226491b95464dcc50cf46c
kevinloormz <kevinloorm@gmail.com>
2021-06-09 11:59:43 -0500

    Update theme template dark o claro

--- lib/core/theme/custom_color.dart
+++ lib/core/theme/custom_color.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:mygetxtemplate/core/utils/getStorageKey.dart';
 
 class CustomColor {
-  var primaryTextColor = GetStorage().read(GetStorageKey.isDarkMode)
-      ? Color(0xDDFFFFFF)
-      : Color(0xDD000000);
-  var secondaryTextColor = GetStorage().read(GetStorageKey.isDarkMode)
-      ? Color(0x89FFFFFF)
-      : Color(0x89000000);
+  var primaryTextColor =
+      GetStorageKey().isSaveDarkMode() ? Color(0xDD000000) : Color(0xDDFFFFFF);
+  var secondaryTextColor =
+      GetStorageKey().isSaveDarkMode() ? Color(0x89FFFFFF) : Color(0x89000000);
   var blackColor = Colors.black;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:mygetxtemplate/core/theme/custom_theme.dart';
+import 'package:mygetxtemplate/core/utils/getStorageKey.dart';
 import 'package:mygetxtemplate/modules/global_binding.dart';
 import 'package:mygetxtemplate/modules/initial/initial_page.dart';
-
-import 'core/utils/getStorageKey.dart';
 import 'routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  final _getStorage = GetStorage();
-  bool _isDarkMode = _getStorage.read(GetStorageKey.isDarkMode) ?? false;
-  _getStorage.write(GetStorageKey.isDarkMode, _isDarkMode);
 
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.INITIAL,
-    theme: _isDarkMode ? CustomTheme.darkThemeData : CustomTheme.lightThemeData,
-    defaultTransition: Transition.fade,
+    theme: CustomTheme.lightThemeData,
+    darkTheme: CustomTheme.darkThemeData,
+    themeMode: GetStorageKey().getThemeMode(),
+    defaultTransition: Transition.cupertino,
     initialBinding: GlobalBinding(),
     getPages: AppPages.pages,
     home: InitialPage(),
